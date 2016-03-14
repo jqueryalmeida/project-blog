310983ff1c080bd432bc901d22cc73c5c5b71edb
maequise <tahitoa.merlin@gmail.com>
2016-03-14 17:47:49 +0100

    Update files

--- include/scripts/admin.js
+++ include/scripts/admin.js
@@ -1,6 +1,5 @@
 var app = angular.module('App', []);
 
-app.controller('PanelAdmin', ['$scope', '$http', function(scope, http)
-{
+app.controller('PanelAdmin', ['$scope', '$http', function (scope, http) {
 	console.log('et');
 }]);
